3e916d22cec708b7999798926ce21ba8713791c7
turtletear <ariqmr123@gmail.com>
2021-03-15 22:16:26 +0700

    change dbase to remote dbase

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const mongoose = require("mongoose");
 //database setup
 var uri = `${config.MONGO_URI}/${config.DB_NAME}`; //local dbase
 var uri2 = config.MONGO_URII.toString(); //remote dbase (mongodb atlas)
-var connect = mongoose.connect(uri, {
+var connect = mongoose.connect(uri2, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
